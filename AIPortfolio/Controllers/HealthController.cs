52c0c1fbef04b5aae2d10d9b4bb218a6e7ebe8db
akhil koppala <akhilkoppala123@gmail.com>
2025-07-31 14:32:31 +0530

    Add project files.

---
+++ AIPortfolio/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "Healthy", time = DateTime.UtcNow });
+    }
+}
